26780072b7e809a9aab9072c8fcd3cea938f9b59
C-Bouthoorn <c_bouthoorn@icloud.com>
2016-03-27 20:09:11 +0200

    SCSS!

--- build.sh
+++ build.sh
@@ -1,8 +1,23 @@
 #!/bin/bash
 
+build() {
+	for f in *.coffee; do
+		if [ -e $f ]; then
+			echo "Building $f"
+			coffee -cb "$f"
+		fi
+	done
+
+	for f in *.scss; do
+		if [ -e $f ]; then
+			echo "Building $f"
+			sass --update -q "$f"
+		fi
+	done
+}
+
+build
+
 cd www
-for f in *.coffee; do
-	echo "Building $f"
-	coffee -cb "$f"
-done
-cd ..
+	build
+	cd ..
